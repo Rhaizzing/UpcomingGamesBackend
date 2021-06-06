230e32e6aa64afa9341bf6e91aa21c8c1c79c920
Helton Reis <heltonkreis@gmail.com>
2021-06-06 01:56:33 -0300

    [feature] Add two helper models to aid in JSONB

---
+++ UpcomingGames.Database/Models/GameUrls.cs
@@ -0,0 +1,23 @@
+namespace UpcomingGames.Database.Models
+{
+	public class GameUrls
+	{
+		public string Official { get; set; }
+		public string Wikia { get; set; }
+		public string Wikipedia { get; set; }
+		public string Facebook { get; set; }
+		public string Twitter { get; set; }
+		public string Twitch { get; set; }
+		public string Instagram { get; set; }
+		public string YouTube { get; set; }
+		public string Iphone { get; set; }
+		public string Ipad { get; set; }
+		public string Android { get; set; }
+		public string Steam { get; set; }
+		public string Reddit { get; set; }
+		public string Itch { get; set; }
+		public string EpicGames { get; set; }
+		public string Gog { get; set; }
+		public string Discord { get; set; }
+	}
+}
---
+++ UpcomingGames.Database/Models/ReleaseDates.cs
@@ -0,0 +1,15 @@
+namespace UpcomingGames.Database.Models
+{
+	public class ReleaseDates
+	{
+		public string FirstReleaseDate { get; set; } = "NA";
+		public string Worldwide { get; set; } = "NA";
+		public string NorthAmerica { get; set; } = "NA";
+		public string Europe { get; set; } = "NA";
+		public string Australia { get; set; } = "NA";
+		public string NewZealand { get; set; } = "NA";
+		public string Japan { get; set; } = "NA";
+		public string China { get; set; } = "NA";
+		public string Asia { get; set; } = "NA";
+	}
+}
