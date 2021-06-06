1afb517083c00cd530295e0aebca74ad66b4c8da
Helton Reis <heltonkreis@gmail.com>
2021-06-06 01:55:40 -0300

    [refactor]{!!!/api} Make the IGameSource interface generic and implement the DTO

--- UpcomingGames.Sources/Interfaces/IGameSource.cs
+++ UpcomingGames.Sources/Interfaces/IGameSource.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using UpcomingGames.Database.DTO;
 using UpcomingGames.Database.Entities;
 
 namespace UpcomingGames.Sources.Interfaces
 {
-	public interface IGameSource
+	public interface IGameSource<T>
 	{
-		Task<Game> GetById(long id);
-		Task<Game> SearchByName(string name);
+		Task<FullGameDto> GetOne(T gameQuery);
+		Task<IEnumerable<FullGameDto>> Search(string searchQuery);
+		Task<IEnumerable<FullGameDto>> GetAll(int page, int itemsPerPage);
 	}
 }
