8f66cb2ca617a93d66e5e6139ad28f7c73c29e49
Helton Reis <heltonkreis@gmail.com>
2021-06-14 17:04:19 -0300

    [feature] Create the game repository to access the database

---
+++ UpcomingGames.API/Repositories/GameRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using UpcomingGames.Database;
+using UpcomingGames.Database.Entities;
+
+namespace UpcomingGames.API.Repositories
+{
+	public class GameRepository
+	{
+		private readonly postgresContext _dbContext;
+
+		public GameRepository(postgresContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public ValueTask<GameEntity?> GetById(int id)
+		{
+			return _dbContext.Games.FindAsync(id);
+		}
+
+		public Task<List<GameEntity>> GetAll()
+		{
+			return _dbContext.Games.ToListAsync();
+		}
+
+		public Task<List<GameEntity>> SearchByName(string name)
+		{
+			return _dbContext.Games.Where(game => game.Name.ToLower().Contains(name.ToLower())).ToListAsync();
+		}
+
+		public ValueTask<EntityEntry<GameEntity>> Add(GameEntity game)
+		{
+			return _dbContext.Games.AddAsync(game);
+		}
+
+		public int SaveChanges()
+			=> _dbContext.SaveChanges();
+	}
+}
